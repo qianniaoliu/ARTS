9c2e1e6b10fda2ef470f8bd2b038aad57652eb88
cdshenlong1 <cdshenlong1@jd.com>
2021-02-03 17:30:20 +0800

    [新增学习webflux]

---
+++ 2021-ARTS/Q1/src/main/java/com/arts/q1/webflux/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.arts.q1.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yusheng
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("/test/requestParam")
+    public String requestParam(@RequestParam String hello){
+        return hello;
+    }
+}
